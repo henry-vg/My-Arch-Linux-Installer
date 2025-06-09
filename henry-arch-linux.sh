02cefa3d5684593c33a941fbea21cf5c1117d242
Henry <hvg.henry@gmail.com>
2025-06-08 23:31:32 -0300

    fix: corrige bootloader, interface gráfica e ferramentas

--- henry-arch-linux.sh
+++ henry-arch-linux.sh
@@ -7,15 +7,15 @@ pause() {
 }
 
 # ----------- Verificação da Conexão com a Internet -----------
-echo "[1/10] Verificando conexão com a internet..."
+echo "[1/12] Verificando conexão com a internet..."
 ping -c 3 google.com > /dev/null 2>&1 || { echo "Sem conexão com a internet. Encerrando..."; exit 1; }
 echo "✓ Conectado."
 pause
 
 # ----------------------- Configurações -----------------------
-echo "[2/10] Configurações iniciais"
+echo "[2/12] Configurações iniciais"
 
-# Escolha da senha do rootMore actions
+# Escolha da senha do root
 echo "Password Rules:"
 echo "  - Must contain at least one letter, one digit, or one special character"
 echo "  - Must be between 3 and 32 characters long."
@@ -91,7 +91,7 @@ done
 pause
 
 # Escolha do disco
-echo "[3/10] Escolha do disco para instalação"
+echo "[3/12] Escolha do disco para instalação"
 echo "Discos disponíveis:"
 lsblk -d -n -p -o NAME,SIZE,TYPE
 while true; do
@@ -140,7 +140,7 @@ pause
 ROOT_END=$((512/1024 + ROOT_SIZE))
 SWAP_END=$((ROOT_END + SWAP_SIZE))
 
-echo "[4/10] Limpando e particionando o disco $DISK..."
+echo "[4/12] Limpando e particionando o disco $DISK..."
 wipefs --all "$DISK"
 parted -s "$DISK" mklabel gpt
 parted -s "$DISK" mkpart ESP fat32 1MiB 512MiB
@@ -152,7 +152,7 @@ echo "✓ Particionamento concluído."
 pause
 
 # Formatação
-echo "[5/10] Formatando particoes..."
+echo "[5/12] Formatando particoes..."
 mkfs.fat -F32 "${DISK}1"
 mkfs.ext4 "${DISK}2"
 mkfs.ext4 "${DISK}4"
@@ -161,7 +161,7 @@ echo "✓ Partições formatadas."
 pause
 
 # Montagem
-echo "[6/10] Montando particoes..."
+echo "[6/12] Montando particoes..."
 mount "${DISK}2" /mnt
 mkdir -p /mnt/boot/efi
 mount "${DISK}1" /mnt/boot/efi
@@ -172,31 +172,68 @@ echo "✓ Partições montadas."
 pause
 
 # Instalação base
-echo "[7/10] Instalando sistema base..."
+echo "[7/12] Instalando sistema base..."
 pacstrap /mnt base linux linux-firmware networkmanager wpa_supplicant sudo dhcpcd
 echo "✓ Sistema base instalado."
 pause
 
 # fstab
-echo "[8/10] Gerando fstab..."
+echo "[8/12] Gerando fstab..."
 genfstab -U /mnt >> /mnt/etc/fstab
 echo "✓ fstab gerado."
 pause
 
 # Configuração via chroot
-echo "[9/10] Entrando no ambiente chroot para configuração..."
+echo "[9/12] Entrando no ambiente chroot para configuração..."
 arch-chroot /mnt /bin/bash <<EOF
-# ...
+pacman -Sy --noconfirm
+ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
+hwclock --systohc
+echo "pt_BR.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+echo "LANG=pt_BR.UTF-8" > /etc/locale.conf
+echo "KEYMAP=br-abnt2" > /etc/vconsole.conf
+echo "$HOSTNAME" > /etc/hostname
+cat <<EOT >> /etc/hosts
+127.0.0.1   localhost.localdomain localhost
+::1         localhost.localdomain localhost
+127.0.1.1   $HOSTNAME.localdomain $HOSTNAME
+EOT
+echo "root:$ROOT_PASSWORD" | chpasswd
+useradd -m -g users -G wheel $USERNAME
+echo "$USERNAME:$USER_PASSWORD" | chpasswd
+echo "$USERNAME ALL=(ALL) ALL" | EDITOR='tee -a' visudo
+systemctl enable NetworkManager
 EOF
 echo "✓ Configuração concluída."
 pause
 
-# Interface gráfica e ferramentas
-echo "[10/10] Instalando interface gráfica e ferramentas..."
+# Instalação do bootloader
+echo "[10/12] Instalando bootloader..."
 arch-chroot /mnt /bin/bash <<EOF
-# ...
+pacman -S grub efibootmgr os-prober --noconfirm
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=grub
+grub-mkconfig -o /boot/grub/grub.cfg
+echo "GRUB_DISABLE_OS_PROBER=false" >> /etc/default/grub
+EOF
+echo "✓ Instalação do bootloader concluída."
+pause
+
+# Interface gráfica
+echo "[11/12] Instalando interface gráfica"
+arch-chroot /mnt /bin/bash <<EOF
+pacman -S --noconfirm xorg-server nvidia nvidia-utils nvidia-prime nvidia-settings mesa gdm gnome-shell gnome-terminal gnome-control-center gnome-tweaks
+systemctl enable gdm
 EOF
 echo "✓ Ambiente gráfico instalado."
+pause
+
+# Instação de ferramentas adicionais
+echo "[12/12] Instalando ferramentas adicionais..."
+arch-chroot /mnt /bin/bash <<EOF
+pacman -S --noconfirm firefox nautilus gimp network-manager-applet networkmanager-openvpn intel-ucode dosfstools ntfs-3g exfat-utils nano git wget curl zip unzip
+EOF
+echo "✓ Ferramentas adicionais instaladas."
 pause
 
 echo "Instalação concluída. Desmontando particoes e reiniciando."
